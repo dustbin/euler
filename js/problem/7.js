1891efb5113e9b299fdc0a889442cba6372e8021
Dustbin <dustbin@localhost.localdomain>
2021-03-20 16:53:11 -0500

    problem 7

--- js/problem/7.js
+++ js/problem/7.js
@@ -1,20 +1,5 @@
-function isprime(i,primes){
-	for(let j=0;j<primes.length;++j){
-		if(primes[j]*primes[j]>i){return true;}
-		if(i%primes[j]==0){return false;}
-	}
-	return true;
-}
-function primen(n){
-	let primes = [];
-	for(let i=2;primes.length<n;++i){
-		if(isprime(i,primes)){
-			primes.push(i);
-		}
-	}
-	return primes[n-1];
-}
 function problem7(){
-	return 1;
+	let prime = new Prime();
+	return prime.getPrime(10000);
 }
 Button.create("problem 7",problem7,104743);
