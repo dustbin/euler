0d7637762b54a6a75316e75c2c980a34a999b3b8
Dustbin <dustbin@localhost.localdomain>
2021-03-19 23:09:58 -0500

    hmmm

--- js/problem/3.js
+++ js/problem/3.js
@@ -1,6 +1,6 @@
 function problem3(){
 	let prime = new Prime();
-	let f = prime.factors(600851475143);
-	return f[f.length-1][0];
+	let factors = prime.factors(600851475143);
+	return factors[factors.length-1][0];
 }
 problem3.answer = 6857;
