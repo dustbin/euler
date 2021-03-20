4ca4d43a38070ef5cf817d8cf36b274c4a2605fd
Dustbin <dustbin@localhost.localdomain>
2021-03-19 23:03:54 -0500

    updating problem 3

--- js/lib/prime.js
+++ js/lib/prime.js
@@ -11,8 +11,28 @@ class Prime {
 	buildPrimes(length){
 		if(this.primes.length>=length){return;}
 		for(let i=this.primes[this.primes.length-1]+1;this.primes.length<length;++i){
-			for(let j=0;this.primes[j]*this.primes[j]<i;++j){
-				if(i%j==0){break;}
+			for(let j=0;;++j){
+				let p = this.primes[j];
+				if(i<p*p){this.primes.push(i);break;}
+				if(i%p==0){break;}
+			}
+		}
+	}
+	factors(n){
+		let ret = [];
+		for(let i=0;;++i){
+			let p = this.getPrime(i);
+			let t = 0;
+			if(n<p*p){
+				if(n>1){ret.push([p,1]);}
+				break;
+			}
+			while(n%p=0){
+				++t;
+				n/=p;
+			}
+			if(t>0){
+				ret.push([p,t]);
 			}
 		}
 	}
--- js/problem/3.js
+++ js/problem/3.js
@@ -1,22 +1,6 @@
-function isprime(i,primes){
-	for(let j in primes){
-		if(primes[j]*primes[j]>i){return true;}
-		if(i%primes[j]==0){return false;}
-	}
-	return true;
+function problem3(){
+	let prime = new Prime();
+	let f = prime.factors(600851475143);
+	return f[f.length-1][0];
 }
-function maxfactor(n){
-	let max = 0;
-	let primes = [];
-	for(let i=2;i*i<n;++i){
-		if(isprime(i,primes)){
-			primes.push(i);
-			while(n%i==0){
-				max = i;
-				n/=i;
-			}
-		}
-	}
-	if(n>1){return n;}
-	return max;
-}
+problem3.answer = 6857;
