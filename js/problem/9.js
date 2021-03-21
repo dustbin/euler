120b32774a7221d412befc02e309e9351a3e2c4a
Dustbin <dustbin@localhost.localdomain>
2021-03-20 21:26:30 -0500

    Problem 9 with some refactoring

--- js/problem/9.js
+++ js/problem/9.js
@@ -1,15 +1,12 @@
-function findTriplet(n){
-	for(let a=1;a<n/3;++a){
-		for(let b=a+1;b<(n-a)/2;++b){
-			let c = Math.sqrt(a*a+b*b);
-			if(a+b+c==n){
+function problem9(){
+	for(let a=1;a<1000/3;++a){
+		for(let b=a+1;b<(1000-a)/2;++b){
+			let c = 1000-a-b;
+			if(a*a+b*b==c*c){
 				return a*b*c;
 			}
 		}
 	}
 	return -1;
-}
-function problem9(){
-	return 1;
 }
 Button.create("problem 9",problem9,31875000);
