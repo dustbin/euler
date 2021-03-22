465778458222592a0694286e6222f32c2695aeee
Dustbin <dustbin@localhost.localdomain>
2021-03-21 19:13:10 -0500

    Problem 12

--- js/problem/12.js
+++ js/problem/12.js
@@ -1,20 +1,11 @@
-function sumn(n){return (n*n+n)/2;}
-function isPrime(i,primes){
-	for(let j=0;j<primes.length;++j){
-		if(primes[j]*primes[j]>i){return true;}
-		if(i%primes[j]==0){return false;}
-	}
-	return true;
-}
-function findDivisors(n,primes){
-	let roots = 0;
-	for(let i=0;i<primes.length;++i){
-		while(n%primes[i]){roots+=1;n/=t;}
-		if(n==1){break;}
-	}
-}
-
 function problem12(){
-	return 1;
+	let prime = new Prime();
+	for(let i = 1;;++i){
+		let t = Summation.ofN(i);
+		let f = prime.factors(t);
+		if(f.reduce((a,c)=>a+a*c,1)>500){
+			return t;
+		}
+	}
 }
 Button.create("problem 12",problem12,null);
