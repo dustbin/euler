cea4c1d3318b4ee45eb9733d2827d99a11b779ae
Dustbin <dustbin@localhost.localdomain>
2021-03-22 21:36:35 -0500

    derp, need the n, not the max

--- js/problem/14.js
+++ js/problem/14.js
@@ -1,10 +1,14 @@
 function problem14(){
 	let collatz = new Collatz();
 	let max = 0;
+	let n = 0;
 	for(let i = 2;i<1000000;++i){
 		let t = collatz.solve(i);
-		if(t>max){max = t;}
+		if(t>max){
+			max = t;
+			n = i;
+		}
 	}
-	return max;
+	return n;
 }
 Button.create("problem 14",problem14,null);
