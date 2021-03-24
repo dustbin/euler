6b6f763a3e6dcafc7ef438b0e50daa38cd6f3036
Dustbin <dustbin@localhost.localdomain>
2021-03-23 20:30:08 -0500

    Adding problem 15

---
+++ js/problem/15.js
@@ -0,0 +1,18 @@
+function problem15(){
+	let grid = [];
+	for(let i=0;i<=20;++i){
+		for(let j=0;j<=20;++j){
+			if(j==0){
+				grid[i][j]=1;
+			}else{
+				if(i==0){
+					grid[i][j]=1;
+				}else{
+					grid[i][j]=grid[i-1][j]+grid[i][j-1];
+				}
+			}
+		}
+	}
+	return grid[20][20];
+}
+Button.create("problem 15",problem15,null);
