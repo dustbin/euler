4239ad31cf0b32209d205aafa0bd2d1990ae693d
Dustbin <dustbin@localhost.localdomain>
2021-03-24 20:39:01 -0500

    I don't know

--- js/problem/16.js
+++ js/problem/16.js
@@ -1,11 +1,11 @@
 function problem16(){
 	let ret = 0;
-	let num = 1n << 1000;
+	let num = 1n << 1000n;
 	while(num>0){
 		ret += num%10;
 		num -= num%10;
 		num /= 10;
 	}
-	return ret.toString();
+	return ret;
 }
 Problem.create("problem 16",problem16,null);
