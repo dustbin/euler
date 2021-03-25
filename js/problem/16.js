966c1ad2e66a8c7ecc6a11574246b1b14fdbdb79
Dustbin <dustbin@localhost.localdomain>
2021-03-24 19:57:44 -0500

    problem 16

---
+++ js/problem/16.js
@@ -0,0 +1,11 @@
+function problem16(){
+	let ret = 0;
+	let num = 1n << 1000;
+	while(num>0){
+		ret += num%10;
+		num -= num%10;
+		num /= 10;
+	}
+	return ret;
+}
+Button.create("problem 16",problem16,null);
