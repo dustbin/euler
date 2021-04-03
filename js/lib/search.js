752613afb01e23b77e9fb7077a6ebfcc977456c7
Dustbin <dustbin@localhost.localdomain>
2021-04-03 18:11:52 -0500

    Problem 26

---
+++ js/lib/search.js
@@ -0,0 +1,42 @@
+class Match{
+	constructor(n0,n1){
+		this.n0 = n0;
+		this.n1 = n1;
+		this.i = 0;
+		this.s = 0;
+	}
+	check(a,compare){
+		let t0 = a[this.n0+this.i];
+		let t1 = a[this.n1+this.i];
+		if(compare(t0,t1)){
+			if(this.n0+this.i==this.n1){
+				this.s = this.i
+				return 2;
+			}
+			return 1;
+		}
+		return 0;
+	}
+	size(){
+		this.s;
+	}
+	increment(){
+		this.i+=1;
+	}
+	static getRepeating(n,d){
+		let a = [];
+		let searches = [];
+		while(n%d!=0){
+			let t = [Math.floor(n/d),n];
+			let j = -1;
+			while(true){
+				j = a.indexOf(n,j+1);
+				if(j<0){break;}
+				searches.push(new Search(j,a.length));
+			}
+			a.push(t);
+			n=(n%i)*10;
+		}
+		return a;
+	}
+}
---
+++ js/problem/26.js
@@ -0,0 +1,9 @@
+function problem26(){
+	let max = 0;
+	for(let i=1;i<1000;++i){
+	}
+}
+problem26.compare(a,b){
+	return a.every((e,i) => e==b[i]);
+}
+Problem.create("problem 26",problem26,null);
