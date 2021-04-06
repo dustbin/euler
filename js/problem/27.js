236ce361902bc060584606bcaa3e37ac8e610876
Dustbin <dustbin@localhost.localdomain>
2021-04-06 17:53:33 -0500

    Starting in the negatives

--- js/problem/27.js
+++ js/problem/27.js
@@ -5,7 +5,7 @@ function problem27(){
 	for(let i=1;;++i){
 		let b = p.getPrime(i);
 		if(b>1000){break;}
-		for(let a=1;a<1000;a+=2){
+		for(let a=-999;a<1000;a+=2){
 			for(let n=0;;++n){
 				if(!p.isPrime(n**2+a*n+b)){
 					if(n>maxN){maxN = n;maxV = a*b;}
