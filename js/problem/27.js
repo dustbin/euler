cb19815af14a0a02280fcf1627c54ceb18eafe5b
Dustbin <dustbin@localhost.localdomain>
2021-04-06 08:45:42 -0500

    Helps to break out

--- js/problem/27.js
+++ js/problem/27.js
@@ -4,8 +4,9 @@ function problem27(){
 	let maxV = 0;
 	for(let i=1;;++i){
 		let b = p.getPrime(i);
+		if(b>1000){break;}
 		for(let a=1;a<1000;a+=2){
-			for(let n=0;n<1000;++n){
+			for(let n=0;;++n){
 				if(!p.isPrime(n**2+a*n+b)){
 					if(n>maxN){maxN = n;maxV = a*b;}
 					break;
