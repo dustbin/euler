3b1464aea450d117cd0ab3163473d45084d79e20
Dustbin <dustbin@localhost.localdomain>
2021-04-07 23:30:59 -0500

    Problem 29

---
+++ js/problem/29.js
@@ -0,0 +1,10 @@
+function problem29(){
+	let d = [];
+	for(let a=2n;a<=100n;++a){
+		for(let b=2n;b<=100n;++b){
+			d[a**b] = true;
+		}
+	}
+	return d.keys.length;
+}
+Problem.create("problem 29",problem29,null);
